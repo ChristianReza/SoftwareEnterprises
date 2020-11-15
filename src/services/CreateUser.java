7d5e76cadd33ba46b9f5bf92b771effcd1215dda
Christian Reza <creza@unomaha.edu>
2020-11-15 13:57:04 -0600

    Create endpoint for user creation

---
+++ src/services/CreateUser.java
@@ -0,0 +1,62 @@
+package services;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import datamodels.dtos.UserDTO;
+import util.DBUtil;
+
+@WebServlet("/createUser")
+public class CreateUser extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public CreateUser() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String firstName = request.getParameter("firstName").trim();
+		String lastName = request.getParameter("lastName").trim();
+		String email = request.getParameter("email");
+		String location = request.getParameter("location");
+		String hobbies = request.getParameter("hobbies");
+		List<String> hobbiesList = Arrays.asList(hobbies.split("")); // split hobbies by a space into a List<String>
+
+		// Create UserDTO from endpoint request
+		UserDTO user = new UserDTO(firstName, lastName, email, location, hobbiesList);
+
+		DBUtil.createUser(user);
+
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String title = "Database Result";
+		String docType = "<!doctype html public \"-//w3c//dtd html 4.0 transitional//en\">\n"; //
+		out.println(docType + //
+				"<html>\n" + //
+				"<head>" + //
+				"<style>table, th, td {\r\n" + "  border: 1px solid black;\r\n" + //
+				"border-collapse: collapse;\r\n  } </style>" + // 
+				"<title>" + title + "</title></head>\n" + //
+				"<body bgcolor=\"#f0f0f0\">\n" + //
+				"<h1 align=\"center\">" + title + "</h1>\n");
+		out.println("<ul>");
+		out.println("<li> Name: " + firstName + " " + lastName);
+		out.println("</ul>");
+//		out.println("<a href=/" + projectName + "/" + searchWebName + ">Search Data</a> <br>");
+		out.println("</body></html>");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+}
